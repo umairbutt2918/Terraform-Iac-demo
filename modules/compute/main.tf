47ca7b82698bdd95695a25cd84f7a3c10ca37c0e
Umair Butt <umairbutt00007@gmail.com>
2026-04-04 00:07:16 +0500

    fix instance type to t3.micro

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "ssh_sg" {
 
 resource "aws_instance" "web" {
  ami = "ami-0c02fb55956c7d316"
- instance_type = "t2.micro"
+ instance_type = "t3.micro"
 
  subnet_id = var.subnet_id
  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
